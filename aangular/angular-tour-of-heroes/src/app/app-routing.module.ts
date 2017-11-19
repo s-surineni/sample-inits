dfaff3cc374b287c5a38d497cf9e3ac59a4628b3
sampath surineni <sampath.surineni@oracle.com>
2017-11-19 22:00:57 +0530

    added dashboard component

--- aangular/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ aangular/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
     // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'heroes', component: HeroesComponent }
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ]
 
 @NgModule({
--- aangular/angular-tour-of-heroes/src/app/app.module.ts
+++ aangular/angular-tour-of-heroes/src/app/app.module.ts
@@ -9,14 +9,16 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
     declarations: [
         AppComponent,
         HeroDetailComponent,
         HeroesComponent,
-        MessagesComponent
+        MessagesComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
