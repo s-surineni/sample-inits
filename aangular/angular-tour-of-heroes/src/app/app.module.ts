dec8e76d9dd772d45038c38e8bb418bda162531d
sampath surineni <sampath.surineni@oracle.com>
2017-11-19 09:59:59 +0530

    added router

--- aangular/angular-tour-of-heroes/src/app/app.module.ts
+++ aangular/angular-tour-of-heroes/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service'
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
     declarations: [
@@ -19,7 +20,8 @@ import { MessageService } from './message.service'
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        AppRoutingModule
     ],
     providers: [HeroService, MessageService],
     bootstrap: [AppComponent]
