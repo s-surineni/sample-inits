840ec3eafbbe24fa02e0c4c1dcd3645d5a3f86ca
sampath surineni <sampath.surineni@oracle.com>
2017-11-20 14:06:06 +0530

    done with the router

--- aangular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ aangular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -15,12 +15,6 @@ export class HeroesComponent implements OnInit {
         this.getHeroes();
     }
 
-    selectedHero: Hero;
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
-
     heroes: Hero[];
 
     getHeroes(): void {
