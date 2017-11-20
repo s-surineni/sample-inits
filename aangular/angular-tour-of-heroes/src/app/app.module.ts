31a6d21f0205c87039a1bb81e722ba328ca05f82
sampath surineni <sampath.surineni@oracle.com>
2017-11-20 16:23:08 +0530

    able to edit the hero

--- aangular/angular-tour-of-heroes/src/app/app.module.ts
+++ aangular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -10,7 +11,9 @@ import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
     declarations: [
@@ -21,6 +24,10 @@ import { DashboardComponent } from './dashboard/dashboard.component'
         DashboardComponent
     ],
     imports: [
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, { dataEncapsulation: false }
+        ),
         BrowserModule,
         FormsModule,
         AppRoutingModule
