e26599340e7732590f84228e9a98f6202e4a6dd2
sampath <sampath.surineni@oracle.com>
2018-06-15 10:07:59 +0530

    added classed for classpath

---
+++ jjava/cclassp/Start.java
@@ -0,0 +1,10 @@
+import apackage.End;
+
+
+class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        End e = new End();
+        e.end();
+    }
+}
---
+++ jjava/cclassp/middle/apackage/End.java
@@ -0,0 +1,7 @@
+package apackage;
+
+public class End {
+    public void end() {
+        System.out.println("This is the end");
+    }
+}
