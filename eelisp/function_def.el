d1175372d92cb26318b9fe73e599fa3ad8327b05
sampath surineni <sampath.skt@gmail.com>
2018-10-02 08:36:16 +0530

    added interactive function

--- eelisp/function_def.el
+++ eelisp/function_def.el
@@ -1,6 +1,12 @@
 ;; The following takes a number as input and multiplies it with 7
 (defun multiply-by-7 (number)
-  "The function divides input by 7"
+  "The function multiplies input by 7"
   (* 7 number))
 
 (multiply-by-7 3)
+
+
+(defun interactive-multiply-by-7 (number)
+     "The function multiplies input by 7 interactively"
+     (interactive "p")
+     (message "The result is %d" (* 7 number)))
