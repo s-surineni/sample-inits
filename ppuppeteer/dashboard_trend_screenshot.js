cf8128bbba7ce6755d7841670d9dd7112c3c09bb
Sampath Surineni <sampath.skt@gmail.com>
2019-06-25 16:26:05 +0530

    waiting until submit button is visible

--- ppuppeteer/dashboard_trend_screenshot.js
+++ ppuppeteer/dashboard_trend_screenshot.js
@@ -5,9 +5,12 @@ const searchable = process.argv.includes('--searchable');
 const avisoURL = 'http://localhost:8090';
 
 (async() => {
-    const browser = await puppeteer.launch({headless: false,
-                                            slowMo: 50});
-    // const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch({headless: false,
+    //                                         slowMo: 100});
+    const browser = await puppeteer.launch();
+
+    // const browser = await puppeteer.launch({headless: false});
+
     const page = await browser.newPage();
     await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2});
     await page.goto(avisoURL);
@@ -21,7 +24,7 @@ const avisoURL = 'http://localhost:8090';
     // Waiting on appearance of SIGN IN button that is disabled
     // had to do this as no new selector is being added or not page load is happening
     // Type password
-    await page.waitForSelector('.button, .submitbtn, .disabled');
+    await page.waitForSelector('.signinnext .button.submitbtn.disabled', {visible: true});
     await page.type('#login_password', process.env.AVISO_PWD);
     await page.waitForSelector('.button.submitbtn.valid');
     await page.screenshot({ path: 'password_page.png' });
@@ -40,6 +43,21 @@ const avisoURL = 'http://localhost:8090';
     await page.waitForSelector('.fm-grid-wrapper', {visible: true});
     await page.waitForSelector('.spinning-loader-overlay', {hidden: true });
     await page.screenshot({path: 'forecast.png'});
+
+    const only_forecast = await page.$('.main-container');
+    await only_forecast.screenshot({path: 'only_forecast.png'});
+
+
+    // now lets switch to dashboard and take picture of trend graph
+    await page.click('.fa.fa-tachometer');
+    await page.waitForNavigation();
+    await page.waitForSelector('.trend', {visible: true});
+    // await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2});
+    // await page.setViewport({width: 1200, height: 800});
+
+    await page.waitFor(3000);
+    const trend = await page.$('.trend');
+    await trend.screenshot({path: 'trend.png'});
 
 
     await browser.close();
