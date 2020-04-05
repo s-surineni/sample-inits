f1522d1fadacb2d373cf10fa5ebb9fb7e9c3ed36
sampath surineni <sampath.skt@gmail.com>
2020-04-05 22:28:27 +0530

    new notebooks

---
+++ eelisp/if_else.el
@@ -0,0 +1,19 @@
+(if 1
+    (print "true")                      ;runs when condition is true
+  (print "false")                       ;runs when condition is false
+  )
+
+
+
+(if nil
+    (print "true")                      ;runs when condition is true
+  (print "false")                       ;runs when condition is false
+  )
+
+
+(if 1
+    ((print "true")
+     (print "st1")
+     (print "st2"))                      ;runs when condition is true
+  (print "false")                       ;runs when condition is false
+  )
