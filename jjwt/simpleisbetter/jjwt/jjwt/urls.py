e78e38157e2988d883c0de2aeb3f73e3c609b6c3
sampath surineni <sampath.skt@gmail.com>
2020-04-29 07:49:34 +0530

    added jwt token authentication too

--- jjwt/simpleisbetter/jjwt/jjwt/urls.py
+++ jjwt/simpleisbetter/jjwt/jjwt/urls.py
@@ -17,9 +17,17 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
+    path('api/token/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
