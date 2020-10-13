b44d67dee3a208ff1991b207ea954ffad631a055
Sampath Surineni <sampath.skt@gmail.com>
2020-10-13 16:46:48 +0530

    added ignore for sqlite3

--- ddjango/multi_user/multi_user/urls.py
+++ ddjango/multi_user/multi_user/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from multiauth.views import SignUpView
+from multiauth.views import SignUpView, student_signup, teacher_signup
 
 urlpatterns = [
-    # path('', include('multiauth.urls')),
+    path('', include('multiauth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
-    path('admin/', admin.site.urls),
+    path('accounts/signup/student/', student_signup, name='student_signup'),
+    path('accounts/signup/teacher/', teacher_signup, name='teacher_signup'),    path('admin/', admin.site.urls),
 ]
--- ddjango/multi_user/multiauth/views.py
+++ ddjango/multi_user/multiauth/views.py
@@ -1,6 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class SignUpView(TemplateView):
     template_name = 'registration/signup.html'
+
+
+def home(request):
+    return HttpResponse('Hello')
+
+
+def student_signup(request):
+    return HttpResponse('Hello')
+
+
+def teacher_signup(request):
+    return HttpResponse('Hello')
