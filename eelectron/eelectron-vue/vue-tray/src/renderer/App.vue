ccf3b700af4fe18d103acb05aacfc8843e199adb
sampath surineni <sampath.surineni@gmail.com>
2021-08-13 12:47:54 +0530

    added src for electron tray

---
+++ eelectron/eelectron-vue/vue-tray/src/renderer/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue-tray'
+  }
+</script>
+import bus from '@/bus'
+  mounted () {
+    bus.$on('ping', () => {
+      // event logic
+      console.log('pong')
+    })
+  }
+<style>
+  /* CSS */
+</style>
